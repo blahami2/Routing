9445b12b892f2318e90a1be2362757d48c750a3f
Michael Bláha <blahami2@gmail.com>
2016-03-07 10:52:06 +0100

    Added TimeMeasurement

---
+++ src/main/java/cz/certicon/routing/utils/measuring/TimeMeasurement.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.utils.measuring;
+
+/**
+ *
+ * @author Michael Blaha {@literal <michael.blaha@certicon.cz>}
+ */
+public class TimeMeasurement {
+    private long start;
+    private long time;
+    
+    public void start(){
+        start = System.nanoTime();
+    }
+    
+    public long stop(){
+        time = getCurrentTimeElapsed();
+        return time;
+    }
+    
+    public long getCurrentTimeElapsed(){
+        return System.nanoTime() - start;
+    }
+}
