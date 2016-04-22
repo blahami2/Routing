ec0f39acad55a25d683cffb53aebf5e52e12a4b9
Michael Bláha <blahami2@gmail.com>
2016-04-22 14:31:34 +0200

    SqliteNodeSearcher done

--- src/main/java/cz/certicon/routing/data/nodesearch/sqlite/SqliteNodeSearcher.java
+++ src/main/java/cz/certicon/routing/data/nodesearch/sqlite/SqliteNodeSearcher.java
@@ -7,17 +7,19 @@ package cz.certicon.routing.data.nodesearch.sqlite;
 
 import cz.certicon.routing.application.algorithm.Distance;
 import cz.certicon.routing.application.algorithm.DistanceFactory;
+import cz.certicon.routing.application.algorithm.EdgeData;
 import cz.certicon.routing.data.basic.database.AbstractEmbeddedDatabase;
+import cz.certicon.routing.data.basic.database.EdgeResultHelper;
 import cz.certicon.routing.data.nodesearch.NodeSearcher;
 import cz.certicon.routing.model.entity.Coordinates;
+import cz.certicon.routing.model.entity.Node;
+import cz.certicon.routing.model.entity.common.SimpleEdgeData;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.sqlite.SQLiteConfig;
 
 /**
@@ -33,23 +35,54 @@ public class SqliteNodeSearcher implements NodeSearcher {
     }
 
     @Override
-    public Map<Coordinates, Distance> findClosestNodes( Coordinates coordinates, DistanceFactory distanceFactory ) throws IOException {
+    public Map<Node.Id, Distance> findClosestNodes( Coordinates coordinates, DistanceFactory distanceFactory, SearchFor searchFor ) throws IOException {
+        Map<Node.Id, Distance> distanceMap = new HashMap<>();
+        final String pointString = "ST_GeomFromText('POINT(" + coordinates.getLongitude() + " " + coordinates.getLatitude() + ")',4326)";
+        final String keyDistanceFromStart = "distance_from_start";
+        final String keyDistanceToEnd = "distance_to_end";
         try {
-            ResultSet rs = database.read( "SELECT n FROM nodes_view n WHERE ST_Equals("
-                    + "ST_SnapToGrid(point, 0.000001),"
+            ResultSet rs = database.read( "SELECT n.id FROM nodes_view n WHERE ST_Equals("
+                    + "ST_SnapToGrid(" + pointString + ", 0.000001),"
                     + "ST_SnapToGrid(n.geom, 0.000001)"
                     + ");" );
             boolean isCrossroad = false;
-            while(rs.next()){ // for all nodes found
-                // TODO stash changes, create new branch, change NodeSearcher specification to node.ids, distances, merge to development, merge from development to this branch
-                
+            while ( rs.next() ) { // for all nodes found
+                isCrossroad = true;
+                long id = rs.getLong( "id" );
+                distanceMap.put( Node.Id.createId( id ), distanceFactory.createZeroDistance() );
             }
-            if(!isCrossroad){
-                
+            if ( !isCrossroad ) {
+                rs = database.read(
+                        "SELECT " + EdgeResultHelper.select( EdgeResultHelper.Columns.IS_FORWARD, EdgeResultHelper.Columns.SOURCE, EdgeResultHelper.Columns.TARGET, EdgeResultHelper.Columns.SPEED, EdgeResultHelper.Columns.IS_PAID, EdgeResultHelper.Columns.LENGTH ) + ", ST_Length(ST_LineSubstring(e.geom, 0, ST_LineLocatePoint(e.geom, ST_ClosestPoint(e.geom, e2.point))), true) AS " + keyDistanceFromStart + ", ST_Length(ST_LineSubstring(e.geom, ST_LineLocatePoint(e.geom, ST_ClosestPoint(e.geom, e2.point)),1), true) AS " + keyDistanceToEnd + " "
+                        + "FROM edges_view e "
+                        + "JOIN ( "
+                        + "	SELECT e.data_id, x.point "
+                        + "        FROM edges_view e "
+                        + "        JOIN (select " + pointString + " AS point) AS x ON true "
+                        // + "        WHERE ST_DWithin(x.point, e.geom, 300) "
+                        + "        ORDER BY e.geom <-> x.point "
+                        + "        LIMIT 1 "
+                        + ") AS e2 "
+                        + "ON e.data_id = e2.data_id;" );
+                while ( rs.next() ) {
+                    EdgeResultHelper edgeResultHelper = new EdgeResultHelper( rs );
+                    EdgeData edgeData = new SimpleEdgeData( edgeResultHelper.getSpeed(), edgeResultHelper.getIsPaid(), edgeResultHelper.getLength() );
+                    Node.Id nodeId;
+                    double length;
+                    if ( searchFor.equals( SearchFor.SOURCE ) ) {
+                        nodeId = Node.Id.createId( edgeResultHelper.getTargetId() );
+                        length = ( edgeResultHelper.getIsForward() ) ? rs.getDouble( keyDistanceToEnd ) : rs.getDouble( keyDistanceFromStart );
+                    } else {
+                        nodeId = Node.Id.createId( edgeResultHelper.getSourceId() );
+                        length = ( edgeResultHelper.getIsForward() ) ? rs.getDouble( keyDistanceFromStart ) : rs.getDouble( keyDistanceToEnd );
+                    }
+                    distanceMap.put( nodeId, distanceFactory.createFromEdgeDataAndLength( edgeData, length ) );
+                }
             }
         } catch ( SQLException ex ) {
             throw new IOException( ex );
         }
+        return distanceMap;
     }
 
     private static class StringDatabase extends AbstractEmbeddedDatabase<ResultSet, String> {
