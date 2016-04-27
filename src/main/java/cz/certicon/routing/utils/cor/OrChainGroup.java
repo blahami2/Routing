76e76a5638aa2ac98c68f69527e1ec7df25e6e64
Michael Bláha <blahami2@gmail.com>
2016-04-27 14:34:22 +0200

    Chain of responsibility partial support

---
+++ src/main/java/cz/certicon/routing/utils/cor/OrChainGroup.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.utils.cor;
+
+/**
+ *
+ * @author Michael Blaha {@literal <michael.blaha@certicon.cz>}
+ * @param <Traveller> traveling instance
+ */
+public class OrChainGroup<Traveller> extends SimpleChainGroup<Traveller> {
+
+        @Override
+        protected boolean executeNext( ChainLink<Traveller> next, Traveller t ) {
+            return next.execute( t );
+        }
+
+    }
