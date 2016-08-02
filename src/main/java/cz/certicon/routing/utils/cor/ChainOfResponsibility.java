ef438ff9bc525caab0fcfbd3f25b4682ac75f021
Michael Bláha <blahami2@gmail.com>
2016-08-02 10:13:34 +0200

    Documentation of utils.cor

--- src/main/java/cz/certicon/routing/utils/cor/ChainOfResponsibility.java
+++ src/main/java/cz/certicon/routing/utils/cor/ChainOfResponsibility.java
@@ -6,15 +6,16 @@
 package cz.certicon.routing.utils.cor;
 
 /**
+ * Need to think this through first. TODO
  *
  * @since 1.X, X &gt; 2
  * @author Michael Blaha {@literal <michael.blaha@certicon.cz>}
- * @param <In> in 
+ * @param <In> in
  * @param <Out> out
  */
 public interface ChainOfResponsibility<In, Out> {
 
     public Out process( In input, Out output );
 
-    public ChainOfResponsibility<In, Out> add( Handler<In,Out> handler );
+    public ChainOfResponsibility<In, Out> add( Handler<In, Out> handler );
 }
